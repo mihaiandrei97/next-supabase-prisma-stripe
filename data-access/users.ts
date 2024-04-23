7ceee00f1dd4e3435e1f26ee8a5a3054f2342205
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2024-04-23 20:30:31 +0300

    re-order users.ts

--- data-access/users.ts
+++ data-access/users.ts
@@ -2,28 +2,6 @@ import { db } from "@/lib/database";
 import { createSupabaseInstance } from "@/lib/supabase";
 import { cookies } from "next/headers";
 
-export async function getUser() {
-  try {
-    const supabase = createSupabaseInstance(cookies());
-    const {data, error} = await supabase.auth.getUser();
-    if (error) {
-      return null;
-    } else {
-      const dbUser = await getOrCreateUserForSession(data.user.id);
-      const user = {
-        email: data.user.email,
-        id: data.user.id,
-        role: dbUser.role,
-        proTier: dbUser.proTier,
-      };
-      return user;
-    }
-  } catch (error) {
-    return null;
-  }
-}
-
-
 export async function getOrCreateUserForSession(userId: string) {
   let user = await db.user.findUnique({
     where: { id: userId },
@@ -47,4 +25,25 @@ export async function getOrCreateUserForSession(userId: string) {
     });
   }
   return user;
+}
+
+export async function getUser() {
+  try {
+    const supabase = createSupabaseInstance(cookies());
+    const { data, error } = await supabase.auth.getUser();
+    if (error) {
+      return null;
+    } else {
+      const dbUser = await getOrCreateUserForSession(data.user.id);
+      const user = {
+        email: data.user.email,
+        id: data.user.id,
+        role: dbUser.role,
+        proTier: dbUser.proTier,
+      };
+      return user;
+    }
+  } catch (error) {
+    return null;
+  }
 }
